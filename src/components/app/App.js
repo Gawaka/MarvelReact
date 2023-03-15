c69f7ff1b2655cb8de77d2c750255a66111964dd
Gawaka <107581246+Gawaka@users.noreply.github.com>
2023-03-15 08:32:56 +0200

    visual

---
+++ src/components/app/App.js
@@ -0,0 +1,60 @@
+import { Component } from "react/cjs/react.production.min";
+import PropTypes from 'prop-types';
+
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+
+import decoration from '../../resources/img/vision.png';
+import img from '../../bg.jpg';
+
+class App extends Component {
+    state = {
+        showRandomChar: true,
+        selectedChar: null
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
+                    <div className="char__content">
+                        <ErrorBoundary>
+                            <CharList onCharSelected={this.onCharSelected}/>
+                        </ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }
+}
+
+App.propTypes = {
+    onCharSelected: PropTypes.func
+}
+
+export default App;
---
+++ src/components/charInfo/CharInfo.js
@@ -0,0 +1,134 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Sckeleton from '../skeleton/Skeleton';
+
+import './charInfo.scss';
+import MarverService from '../../services/MarvelService';
+
+class CharInfo extends Component {
+
+    state = {
+        char: null,
+        loading: false,
+        error: false
+    }
+
+    marvelService = new MarverService();
+
+    componentDidMount() {
+        this.updateChar();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.charId !== prevProps.charId) {
+            this.updateChar();
+        }
+    }
+
+    updateChar = () => {
+        const {charId} = this.props;
+        if (!charId) {
+            return;
+        }
+
+        this.onCharloading();
+
+        this.marvelService
+            .getCharacters(charId)
+            .then(this.onCharloaded)
+            .catch(this.onError);
+    }
+
+    onCharloaded = (char) => {
+        this.setState({
+            char, 
+            loading: false
+        })
+    }
+
+    onCharloading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    render() {
+        const {char, loading, error} = this.state;
+
+        const skeleton = char || loading || error ? null : <Sckeleton/>;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error || !char) ? <View char={char}/> : null;
+
+        return (
+            <div className="char__info">
+                {skeleton}
+                {errorMessage}
+                {spinner}
+                {content}
+            </div>
+        )
+    }
+}
+
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
+
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
+    return (
+        <>
+            <div className="char__basics">
+            <img src={thumbnail} alt={name} style={imgStyle}/>
+            <div>
+                <div className="char__info-name">{name}</div>
+                <div className="char__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">Homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div className="char__descr">
+            {description}
+        </div>
+        <div className="char__comics">Comics:</div>
+        <ul className="char__comics-list">
+            {comics.length > 0 ? null : 'There is no comics with this character.'}
+            {
+                comics.map((item, i) => {
+                    if (i > 9) return;
+                    return (
+                        <li className="char__comics-item" key={i}>
+                            {item.name}
+                        </li>
+                    )
+                })
+            }
+        </ul>
+        </>
+    )
+
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
+
+export default CharInfo;
